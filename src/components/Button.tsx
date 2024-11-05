a612be5ac694dc4d8acf481ee7c1d8b4870670b2
Yohann Gusso <yohann.gusso@hotmail.com>
2024-11-04 22:31:58 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const MyButton: React.FC = () => {
+  return <Button variant="contained">My Button</Button>;
+};
+
+export default MyButton;
